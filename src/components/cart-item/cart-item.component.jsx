20bb4c7cfe2435b6b1ffdb955b06523d7c92c6ac
Manuel Pena <132922655+MannyCyberBot@users.noreply.github.com>
2024-06-07 15:05:41 -0400

    add to cart functionality

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,16 @@
 import "./cart-item.style.scss";
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+  const { name, imageUrl, quantity, price } = cartItem;
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
   );
 };
